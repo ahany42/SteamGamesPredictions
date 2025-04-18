91067b2d4ac3b45a44c0a4718a575686b25dc2a7
ahany42 <ahanyfathy42@gmail.com>
2025-04-18 14:27:51 +0200

    Combine Dataset Modified

--- utils/utils.py
+++ utils/utils.py
@@ -4,6 +4,11 @@ def combine_datasets():
     dlcs = pd.read_csv("datasets/raw/dlcs.csv")
     gamalytic = pd.read_csv("datasets/raw/gamalytic_steam_games.csv")
     info = pd.read_csv("datasets/raw/info_base_games.csv", low_memory=False)
+    
+    cols = [col for col in gamalytic.columns if col != 'reviewScore']
+    cols.append('reviewScore')
+    gamalytic = gamalytic[cols]
+       
 
     demos.columns = demos.columns.str.strip()
     dlcs.columns = dlcs.columns.str.strip()
@@ -20,14 +25,15 @@ def combine_datasets():
     gamalytic["Full_game_appid"] = gamalytic["Full_game_appid"].astype(str)
     info["Full_game_appid"] = info["Full_game_appid"].astype(str)
 
-    merged_df = demos.merge(dlcs, on="Full_game_appid", how="outer") \
-                    .merge(gamalytic, on="Full_game_appid", how="outer") \
-                    .merge(info, on="Full_game_appid", how="outer")
+    merged_df = demos.merge(dlcs, on="Full_game_appid", how="left") \
+                    .merge(gamalytic, on="Full_game_appid", how="right") \
+                    .merge(info, on="Full_game_appid", how="inner")
 
+    cols = [col for col in merged_df.columns if col != 'reviewScore']
+    cols.append('reviewScore')
+    merged_df = merged_df[cols]
     merged_df.to_csv("datasets/raw/combined_games.csv", index=False)
     print("Demos:", demos.shape)
     print("DLCs:", dlcs.shape)
     print("Gamalytic:", gamalytic.shape)
     print("Merged shape:",merged_df.shape)
-    if 'reviewScore' in merged_df.columns:
-        review_score = merged_df.pop('reviewScore')
